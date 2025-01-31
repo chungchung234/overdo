787d1d12c740a32c26ba581fec2389cfb4f93218
이충현 <a8207637@naver.com>
2025-02-01 07:25:42 +0900

    init project

---
+++ src/main/java/se/overdo/OverdoApplication.java
@@ -0,0 +1,13 @@
+package se.overdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverdoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/overdo/OverdoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.overdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
